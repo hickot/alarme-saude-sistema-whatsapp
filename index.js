b4c7bbc51f82af185a0ef5f559f7318ee6fa89d8
hickot <bydouglascontato@gmail.com>
2025-10-26 18:17:36 -0300

    modificando o código para alertar o sistema e realizar o envio de mensagem da cotação do Bitcoin

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 const fs = require('fs');
 
 // Caminho do arquivo onde será salvo
-const caminhoArquivo = './data-hora-saude-sistema.txt';
+const caminhoArquivoBitcoin = './data-hora.txt';
+const caminhoArquivoSaudeSistema = './data-hora-saude-sistema.txt';
+const caminhoValorCotacao = './valor.txt';
 
 const INTERVALO = 30 * 1000; // Checagem a cada minuto
 
 const repeticoes = 3;
 const intervaloEnvio = 10 * 60 * 1000; // 22 minutos em milissegundos
 
+const MOEDAS = [
+  { symbol: "BTCUSDT", limite: 117532.16 },
+];
+
 //contador
 var contador = 0;
-
-// for (let i = 1; i <= repeticoes; i++) {
-//   setTimeout(async () => {
-//     // chama a função e envia a mensagem
-//     await send('Douglas Alvares', mensagem);
-//   }, (i - 1) * intervaloEnvio);
-// }
+var valor = 0;
 
 rotina();
 
@@ -33,52 +33,119 @@ function formatarDataLocal(date) {
   return `${ano}-${mes}-${dia} ${hora}:${minuto}:${segundo}`;
 }
 
-// ===============================
-// Monitoramento Binance
-// ===============================
 async function rotina() {
   console.log("");
   setInterval(async () => {
     try {
 
-      contador = contador + 1
+      for (const moeda of MOEDAS) {
 
-      const mensagem = 'Sistema em funcionamento... ';
+        fs.readFile(caminhoValorCotacao, 'utf8', (erro, dados) => {
+          if (erro) {
+            console.error('Erro ao ler o arquivo:', erro);
+            return;
+          }
 
-      // Ler a data salva no arquivo
-      const conteudo = fs.readFileSync(caminhoArquivo, 'utf8');
-      const inicio = new Date(conteudo);
-      console.log('data inicio:', inicio);
+          // Remove espaços, quebras de linha, e converte para número real
+          const valor = parseFloat(dados.trim().replace(',', '.'));
 
-      // Data atual como objeto Date
-      const fim = new Date();
-      console.log('data atual:', fim);
+          if (isNaN(valor)) {
+            console.error('O conteúdo do arquivo não é um número válido.');
+          } else {
+            console.log('Número lido:', valor);
+            moeda.limite = valor;
+          }
+        });
 
-      // Diferença em milissegundos
-      const diffMs = fim.getTime() - inicio.getTime();
-      console.log('diffMs:', diffMs);
+        console.log("🔍 Monitorando moedas:");
+        MOEDAS.forEach(m => console.log(` - ${m.symbol} <= ${m.limite}`));
+        console.log("");
 
-      // Verifica se já passou o intervalo (ex: 30 minutos)
-      const interval = 60 * 60 * 1000; // 60 minutos em ms
-      console.log('intervalo: ', interval);
+        const res = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${moeda.symbol}`, { timeout: 10000 });
 
-      if (diffMs >= interval) {
-      console.log('✅ Passou o interval, enviando mensagem');
+        if (!res.ok) throw new Error(`HTTP ${res.status}`);
+        const data = await res.json();
+        const preco = parseFloat(data.price);
+        console.log(`[${new Date().toLocaleString()}] ${moeda.symbol}: ${preco} (limite ${moeda.limite})`);
 
-      // Atualiza o arquivo com a data e hora no horário local
-      const dataHoraFormatada = formatarDataLocal(fim);
-      fs.writeFileSync(caminhoArquivo, dataHoraFormatada, 'utf8');
-      console.log('✅ Data e hora atualizada no arquivo!');
+        contador = contador + 1
 
-      // Chama a função de envio
-      const { send } = require('./whatsapp');
-      await send('Douglas Alvares', mensagem);
+        if (Number.isFinite(preco) && preco <= moeda.limite) {
+          const mensagem = `🚨 *Alerta Binance!*\n\n💸 ${moeda.symbol} atingiu *${preco.toLocaleString("pt-BR")}* (limite: ${moeda.limite}).\n📅 ${new Date().toLocaleString("pt-BR")}`;
+
+          console.log('- Passou pela regra da Cotação -')
+
+          // Ler a data salva no arquivo
+          const conteudo = fs.readFileSync(caminhoArquivoBitcoin, 'utf8');
+          const inicio = new Date(conteudo);
+          console.log('data inicio:', inicio);
+
+          // Data atual como objeto Date
+          const fim = new Date();
+          console.log('data atual:', fim);
+
+          // Diferença em milissegundos
+          const diffMs = fim.getTime() - inicio.getTime();
+          console.log('diffMs:', diffMs);
+
+          // Verifica se já passou o intervalo (ex: 30 minutos)
+          const interval = 30 * 60 * 1000; // 60 minutos em ms
+          console.log('intervalo: ', interval);
+
+          if (diffMs >= interval) {
+            console.log('✅ Passou o interval, enviando mensagem');
+
+            // Atualiza o arquivo com a data e hora no horário local
+            const dataHoraFormatada = formatarDataLocal(fim);
+            fs.writeFileSync(caminhoArquivoBitcoin, dataHoraFormatada, 'utf8');
+            console.log('✅ Data e hora atualizada no arquivo!');
+
+            // Chama a função de envio
+            const { send } = require('./whatsapp');
+            await send('Douglas Alvares', mensagem);
+          }
+        } else {
+
+          console.log('- Passou pela regra do Sistema -')
+
+          const mensagem = 'Sistema em funcionamento... ';
+
+          // Ler a data salva no arquivo
+          const conteudo = fs.readFileSync(caminhoArquivoSaudeSistema, 'utf8');
+          const inicio = new Date(conteudo);
+          console.log('data inicio:', inicio);
+
+          // Data atual como objeto Date
+          const fim = new Date();
+          console.log('data atual:', fim);
+
+          // Diferença em milissegundos
+          const diffMs = fim.getTime() - inicio.getTime();
+          console.log('diffMs:', diffMs);
+
+          // Verifica se já passou o intervalo (ex: 30 minutos)
+          const interval = 60 * 60 * 1000; // 60 minutos em ms
+          console.log('intervalo: ', interval);
+
+          if (diffMs >= interval) {
+            console.log('✅ Passou o interval, enviando mensagem');
+
+            // Atualiza o arquivo com a data e hora no horário local
+            const dataHoraFormatada = formatarDataLocal(fim);
+            fs.writeFileSync(caminhoArquivoSaudeSistema, dataHoraFormatada, 'utf8');
+            console.log('✅ Data e hora atualizada no arquivo!');
+
+            // Chama a função de envio
+            const { send } = require('./whatsapp');
+            await send('Douglas Alvares', mensagem);
+          }
+        }
       }
 
-        console.log('Contador: ', contador);
-      
+      console.log('Contador: ', contador);
+
     } catch (err) {
-      
+
       console.error(`❌ Erro consultando `, err && err.message ? err.message : err);
     }
   }, INTERVALO);
